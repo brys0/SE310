f68c4612cbf88e18885a4a7b045efc9ac06ab4a6
DanMil-hub <deathblastselfie@gmail.com>
2026-04-23 10:44:00 -0500

    Added Negative Sqrt Case

--- test_main.py
+++ test_main.py
@@ -11,9 +11,9 @@ class MainTest(unittest.TestCase):
     def test_divide_by_zero(self):
         self.assertEqual(division(4, 0), 0)
 
-        def test_negative_sqrt(self):
+    def test_negative_sqrt(self):
         self.assertEqual(sqrt(-9))
-        
+
     def test_int_division(self):
         self.assertEqual(int_division(2, 3), 0)
 
